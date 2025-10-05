730341cfede3febf7c4e8df74ea88dcf0075f36e
n-ce <abrocks1120@gmail.com>
2025-10-06 00:14:35 +0530

    update

--- api/tracks.ts
+++ api/tracks.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { getSimilarTracks } from './lastfm_api.ts';
-import { getYouTubeSong, type YouTubeSong } from './get_youtube_song.ts';
+import { getSimilarTracks } from './lastfm_api';
+import { getYouTubeSong, type YouTubeSong } from './get_youtube_song';
 
 export default async function handler(
   request: VercelRequest,
