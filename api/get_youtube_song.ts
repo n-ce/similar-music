8c69df27db348cc9ab36c0e7fb328fd3f6dbee30
n-ce <abrocks1120@gmail.com>
2025-10-06 11:12:16 +0530

    improve yt search parser

--- api/get_youtube_song.ts
+++ api/get_youtube_song.ts
@@ -41,7 +41,6 @@ export async function getYouTubeSong(query: string): Promise<YouTubeSong | {}> {
     const contents = data.contents?.tabbedSearchResultsRenderer?.tabs[0]?.tabRenderer?.content?.sectionListRenderer?.contents;
 
     const isMixed = contents?.length === 2;
-
     const searchList = contents?.[1]?.musicShelfRenderer?.contents || [];
 
     for (const item of searchList) {
@@ -60,20 +59,81 @@ export async function getYouTubeSong(query: string): Promise<YouTubeSong | {}> {
 
       if (isMixed && metadataRuns[0].text !== 'Song') continue;
 
+
       const artistRun = metadataRuns.find((run: any) => run.navigationEndpoint?.browseEndpoint?.browseId);
       const author = artistRun?.text;
       const channelId = artistRun?.navigationEndpoint?.browseEndpoint?.browseId;
 
       const duration = metadataRuns.find((run: any) => run.text?.[run.text?.length - 3] === ':')?.text;
-      if (!duration) continue;
 
-      return {
-        id,
-        title,
-        author: author + ' - Topic',
-        duration,
-        channelUrl: 'channel/' + channelId
-      };
+      if (duration)
+        return {
+          id,
+          title,
+          author: author + ' - Topic',
+          duration,
+          channelUrl: 'channel/' + channelId
+        };
+    }
+
+    if (isMixed) {
+      const shelf = contents?.[0]?.musicCardShelfRenderer;
+
+      if ('contents' in shelf) {
+        for (const key in shelf.contents) {
+
+          const fcs = shelf.contents[key]?.musicResponsiveListItemRenderer?.flexColumns;
+          if (!fcs) continue;
+
+          const fcx = fcs[1]?.musicResponsiveListItemFlexColumnRenderer;
+          const details = fcx?.text?.runs;
+          const isSong = details?.[0].text === 'Song';
+          if (!isSong) continue;
+
+          const titleColumnRenderer = fcs?.[0]?.musicResponsiveListItemFlexColumnRenderer;
+          const titleRun = titleColumnRenderer?.text?.runs?.[0];
+          const title = titleRun?.text;
+          const id = titleRun?.navigationEndpoint?.watchEndpoint?.videoId;
+
+          const authorRun = details?.[2];
+          const author = authorRun?.text;
+          const channelId = authorRun?.navigationEndpoint?.browseEndpoint?.browseId;
+          const duration = details?.[4]?.text;
+          if (duration.includes(':') && channelId)
+            return {
+              id,
+              title,
+              author: author + ' - Topic',
+              duration,
+              channelUrl: 'channel/' + channelId
+            };
+
+
+        }
+
+      }
+
+
+      const subtitle = shelf?.subtitle?.runs;
+
+      if (subtitle?.[0].text !== 'Song') {
+      }
+
+      const id = shelf?.title?.runs?.[0]?.navigationEndpoint?.watchEndpoint?.videoId;
+      const title = shelf?.title?.runs?.[0]?.text;
+      const author = subtitle?.[2]?.text;
+      const duration = subtitle?.[4]?.text;
+      const channelId = shelf?.menu?.menuRenderer?.items?.[6]?.menuNavigationItemRenderer?.navigationEndpoint?.browseEndpoint?.browseId;
+
+      if (channelId)
+        return {
+          id,
+          title,
+          author: author + ' - Topic',
+          duration,
+          channelUrl: 'channel/' + channelId
+        };
+
     }
 
 
